0206d39c10de12dc4947ef729f831d856c7af171
rubicscuber <nathanielroberts96@gmail.com>
2025-02-17 11:08:04 -0500

    More commenting and formatting

--- Lab3_MovingLed/MovingLed.vhd
+++ Lab3_MovingLed/MovingLed.vhd
@@ -14,104 +14,104 @@ use IEEE.numeric_std.all;
 ------------------------------------------------------------
 
 entity MovingLed is
-  port (
-    leftButton  : in std_logic;
-    rightButton : in std_logic;
-    resetButton : in std_logic;
-    clock : in std_logic;
+    port (
+        leftButton  : in std_logic;
+        rightButton : in std_logic;
+        resetButton : in std_logic;
+        clock : in std_logic;
 
-    position    : out std_logic_vector(3 downto 0)
-  );
+        position    : out std_logic_vector(3 downto 0)
+    );
 end MovingLed;
 
 architecture MovingLed_ARCH of MovingLed is
 
-  signal inputState : std_logic;
+    signal inputState : std_logic;
 
-  --shift register made from two seperate registers
-  --reg0InputState is updated in SCAN_INPUT_STATE
-  --reg1InputState gets shifted with reg0InputState in UPDATE_POSITION
-  signal reg0InputState : std_logic := '0';
-  signal reg1InputState : std_logic := '1';
+    --shift register made from two seperate registers
+    --reg0InputState is updated in SCAN_INPUT_STATE
+    --reg1InputState gets shifted with reg0InputState in UPDATE_POSITION
+    signal reg0InputState : std_logic := '0';
+    signal reg1InputState : std_logic := '1';
 
-  signal regLeft : std_logic;
-  signal regRight : std_logic;
-  signal regPosition : unsigned (3 downto 0) := "0000";
+    signal regLeft : std_logic;
+    signal regRight : std_logic;
+    signal regPosition : unsigned (3 downto 0) := "0000";
 
-  constant DELAY_COUNT : integer := 1000000; --10ms on a 100MHz clock (0.01s) = 1M
-  signal counter : integer range 0 to DELAY_COUNT;
+    constant DELAY_COUNT : integer := 1000000; --10ms on a 100MHz clock (0.01s) = 1M
+    signal counter : integer range 0 to DELAY_COUNT;
 
 
 begin
 
-  --async signal thats used in SCAN_INPUT_STATE to update reg0InputState
-  inputState <= leftButton or rightButton; 
+--async signal thats used in SCAN_INPUT_STATE to update reg0InputState
+inputState <= leftButton or rightButton; 
 
-  ------------------------------------------------------------
-  --Updates a register of the inputstate once per button press
-  --buttons pressed suimultaniously wont do anything
-  ------------------------------------------------------------
-  SCAN_INPUT_STATE : process(clock)
-  begin
-    if rising_edge(clock) then
-      if inputState /= reg0InputState and counter < DELAY_COUNT then
-        counter <= counter + 1;
-      elsif counter = DELAY_COUNT then
-        reg0InputState <= inputState;
-        counter <= 0;
-      else
-        counter <= 0;
-      end if;
-    end if;
-  end process;
-
-  UPDATE_POSITION : process(clock, resetButton)
-  begin
-    if resetButton = '1' then
-      regPosition <= (others => '0');
-    elsif rising_edge(clock) then
-
-      --shift registers each clock cycle
-      reg1InputState <= reg0InputState;
-
-      --check the state of registers after shift
-      if (reg0InputState = '1') and (reg1InputState = '0') then
-
-        --check if within boundary and the inputs are correct for that direction
-        if (regPosition < 15) and (regLeft = '1') and (regRight = '0') then
-          regPosition <= regPosition + 1;
-        elsif (regPosition > 0) and (regLeft = '0') and (regRight = '1') then
-          regPosition <= regPosition - 1;
-        else
-          regPosition <= regPosition + 0;
+    ------------------------------------------------------------
+    --Updates a register of the inputstate once per button press
+    --buttons pressed suimultaniously wont do anything
+    ------------------------------------------------------------
+    SCAN_INPUT_STATE : process(clock)
+    begin
+        if rising_edge(clock) then
+            if inputState /= reg0InputState and counter < DELAY_COUNT then
+                counter <= counter + 1;
+            elsif counter = DELAY_COUNT then
+                reg0InputState <= inputState;
+                counter <= 0;
+            else
+                counter <= 0;
+            end if;
         end if;
-      end if;
-    end if;
-  end process;
+    end process;
 
-  UPDATE_LEFT_REG : process(clock)
-  begin
-    if rising_edge(clock) then
-      if leftButton = '1' then
-        regLeft <= '1';
-      else
-        regLeft <= '0';
-      end if;
-    end if;
-  end process;
+    UPDATE_POSITION : process(clock, resetButton)
+    begin
+        if resetButton = '1' then
+            regPosition <= (others => '0');
+        elsif rising_edge(clock) then
 
-  UPDATE_RIGHT_REG : process(clock)
-  begin
-    if rising_edge(clock) then
-      if rightButton = '1' then
-        regRight <= '1';
-      else
-        regRight <= '0';
-      end if;
-    end if;
-  end process;
+            --shift registers each clock cycle
+            reg1InputState <= reg0InputState;
 
-  position <= std_logic_vector(regPosition);
+            --due to prop delay, these registers will be in this state before shift occurs
+            if (reg0InputState = '1') and (reg1InputState = '0') then
+
+                --check if within boundary and the inputs are correct for that direction
+                if (regPosition < 15) and (regLeft = '1') and (regRight = '0') then
+                    regPosition <= regPosition + 1;
+                elsif (regPosition > 0) and (regLeft = '0') and (regRight = '1') then
+                    regPosition <= regPosition - 1;
+                else
+                    regPosition <= regPosition + 0;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    UPDATE_LEFT_REG : process(clock)
+    begin
+        if rising_edge(clock) then
+            if leftButton = '1' then
+                regLeft <= '1';
+            else
+                regLeft <= '0';
+            end if;
+        end if;
+    end process;
+
+    UPDATE_RIGHT_REG : process(clock)
+    begin
+        if rising_edge(clock) then
+            if rightButton = '1' then
+                regRight <= '1';
+            else
+                regRight <= '0';
+            end if;
+        end if;
+    end process;
+
+    position <= std_logic_vector(regPosition);
 
 end architecture MovingLed_ARCH;
 
--- Lab3_MovingLed/MovingLed_Basys3.vhd
+++ Lab3_MovingLed/MovingLed_Basys3.vhd
@@ -13,118 +13,117 @@ use IEEE.numeric_std.all;
 --      
 ------------------------------------------------------------
 entity MovingLed_Basys3 is
-  port(
-    btnL : in std_logic;
-    btnR : in std_logic;
-    btnC : in std_logic;
-    clk  : in std_logic;
+    port(
+        btnL : in std_logic;
+        btnR : in std_logic;
+        btnC : in std_logic;
+        clk  : in std_logic;
 
-    an : out std_logic_vector(3 downto 0);
-    seg: out std_logic_vector(6 downto 0);
-    led: out std_logic_vector(15 downto 0)
-  );
+        an : out std_logic_vector(3 downto 0);
+        seg: out std_logic_vector(6 downto 0);
+        led: out std_logic_vector(15 downto 0)
+    );
 end entity MovingLed_Basys3;
 
 
 architecture MovingLed_Basys3_ARCH of MovingLed_Basys3 is
 
-  component MovingLed is
-    port (
-      leftButton  : in std_logic;
-      rightButton : in std_logic;
-      resetButton : in std_logic;
-      clock : in std_logic;
+    component MovingLed is
+        port (
+            leftButton  : in std_logic;
+            rightButton : in std_logic;
+            resetButton : in std_logic;
+            clock : in std_logic;
 
-      position : out std_logic_vector(3 downto 0)
-    );
-  end component;
+            position : out std_logic_vector(3 downto 0)
+        );
+    end component;
 
-  component DigitSplitter is
-    port (
-      position: in std_logic_vector(3 downto 0);
-      --digits (11 downto 8) represent a hex number (seg[3])
-      --seg[3] is blank
-      --digits (7 downto 4) will be the tens place (seg[1])
-      --digits (3 donwto 0) will be the ones place (seg[0])
-      digits  : out std_logic_vector(11 downto 0)
-    );
-  end component;
+    component DigitSplitter is
+        port (
+            position: in std_logic_vector(3 downto 0);
+            --note seg[3] is not decoded here, 
+            --instead the position is fed directly to the SevenSegmentDriver
+            --seg[2] is blank
+            --digits (7 downto 4) will be the tens place (seg[1])
+            --digits (3 donwto 0) will be the ones place (seg[0])
+            digits  : out std_logic_vector(7 downto 0)
+        );
+    end component;
 
-  component BarLedDriver is
-    port (
-      position : in std_logic_vector(3 downto 0);
-      bar : out std_logic_vector(15 downto 0)
-    );
-  end component;
+    component BarLedDriver is
+        port (
+            position : in std_logic_vector(3 downto 0);
+            bar : out std_logic_vector(15 downto 0)
+        );
+    end component;
 
-  component SevenSegmentDriver is
-    port(
-      reset: in std_logic;
-      clock: in std_logic;
+    component SevenSegmentDriver is
+        port(
+            reset: in std_logic;
+            clock: in std_logic;
 
-      digit3: in std_logic_vector(3 downto 0);    --leftmost digit
-      digit2: in std_logic_vector(3 downto 0);    --2nd from left digit
-      digit1: in std_logic_vector(3 downto 0);    --3rd from left digit
-      digit0: in std_logic_vector(3 downto 0);    --rightmost digit
+            digit3: in std_logic_vector(3 downto 0);    --leftmost digit
+            digit2: in std_logic_vector(3 downto 0);    --2nd from left digit
+            digit1: in std_logic_vector(3 downto 0);    --3rd from left digit
+            digit0: in std_logic_vector(3 downto 0);    --rightmost digit
 
-      blank3: in std_logic;    --leftmost digit
-      blank2: in std_logic;    --2nd from left digit
-      blank1: in std_logic;    --3rd from left digit
-      blank0: in std_logic;    --rightmost digit
+            blank3: in std_logic;    --leftmost digit
+            blank2: in std_logic;    --2nd from left digit
+            blank1: in std_logic;    --3rd from left digit
+            blank0: in std_logic;    --rightmost digit
 
-      sevenSegs: out std_logic_vector(6 downto 0);    --MSB=g, LSB=a
-      anodes:    out std_logic_vector(3 downto 0)    --MSB=leftmost digit
-    );
-  end component;
+            sevenSegs: out std_logic_vector(6 downto 0);    --MSB=g, LSB=a
+            anodes:    out std_logic_vector(3 downto 0)    --MSB=leftmost digit
+        );
+    end component;
 
-  signal digit0Signal : std_logic_vector(3 downto 0);
-  signal digit1Signal : std_logic_vector(3 downto 0);
-  signal digit2Signal : std_logic_vector(3 downto 0);
-  signal digit3Signal : std_logic_vector(3 downto 0);
-  signal positionSignal : std_logic_vector(3 downto 0);
-  constant ACTIVE : std_logic := '1';
+    signal digit0Signal : std_logic_vector(3 downto 0);
+    signal digit1Signal : std_logic_vector(3 downto 0);
+    signal digit2Signal : std_logic_vector(3 downto 0);
+    signal positionSignal : std_logic_vector(3 downto 0);
+    constant ACTIVE : std_logic := '1';
 
 begin
     
-  digit2Signal <= "0000"; --the unused digit on the seven seg display
+    digit2Signal <= "0000"; --the unused digit on the seven seg display
 
-  MOVE_LED : MovingLed port map(
-    leftButton => btnL,
-    rightButton=> btnR,
-    resetButton => btnC,
-    clock => clk,
+    MOVE_LED : MovingLed port map(
+        leftButton => btnL,
+        rightButton=> btnR,
+        resetButton => btnC,
+        clock => clk,
 
-    position => positionSignal
-  );
+        position => positionSignal
+    );
 
-  DIGIT_SPLITTER : DigitSplitter port map(
-    position => positionSignal,
-    digits(11 downto 8) => digit3Signal,
-    digits(7 downto 4) => digit1Signal,
-    digits(3 downto 0) => digit0Signal
-  );
+    DIGIT_SPLITTER : DigitSplitter port map(
+        position => positionSignal,
+        digits(7 downto 4) => digit1Signal,
+        digits(3 downto 0) => digit0Signal
+    );
 
-  BAR_LED : BarLedDriver port map(
-    position => positionSignal,
-    bar => led
-  );
+    BAR_LED : BarLedDriver port map(
+        position => positionSignal,
+        bar => led
+    );
 
-  SEVEN_SEGMENT : SevenSegmentDriver port map( --error unbound
-    reset => btnC,
-    clock => clk,
+    SEVEN_SEGMENT : SevenSegmentDriver port map(
+        reset => btnC,
+        clock => clk,
 
-    digit3 => digit3Signal,
-    digit2 => digit2Signal,
-    digit1 => digit1Signal,
-    digit0 => digit0Signal,
+        digit3 => positionSignal, --positionSignal is a straight binary number
+        digit2 => digit2Signal,
+        digit1 => digit1Signal,
+        digit0 => digit0Signal,
 
-    blank3 => (not ACTIVE),
-    blank2 => (ACTIVE),
-    blank1 => (not ACTIVE),
-    blank0 => (not ACTIVE),
+        blank3 => (not ACTIVE),
+        blank2 => (ACTIVE), --blank out seg[2]
+        blank1 => (not ACTIVE),
+        blank0 => (not ACTIVE),
 
-    sevenSegs => seg,
-    anodes => an
-  );
+        sevenSegs => seg,
+        anodes => an
+    );
 
 end architecture MovingLed_Basys3_ARCH;
